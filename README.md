cad7c94bcec1dbeb5d629b8d6474201d34612156
Brittany-Potato <Brittanycc@missionreadyhq.com>
2025-05-27 16:06:47 +1200

    Added more information and key functions/methods for jest

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 A folder I started putting together to act as a JavaScript Guide/Cheat sheet, still a wip with more to come.
 
-Somethings includeded are:
-- Methods, classes, properties etc
-- Basic function variations
-- Simple server blueprint with explinations on each section
-- Jest example file has been started
-- Different imports and installs that are common with Node.js
+Different files include:
+
+- functions.js - A variety of different functions and how to write them
+- imports-installs-whatDo - Different installs and imports you may want to include in your projects and what they do
+- methods-expressions-properties.js - All the different methods, objects, properties, classes etc and how to use them
+- simple-server.js - A simple server blueprint for node.js and explinations on each section
+- test.js - Information about jest, how to run tests and how they should look
--- test.js
+++ test.js
@@ -1,11 +1,37 @@
 //! Jest
 
-JavaScript;
+//JavaScript;
 
 //! Example test
 
-test("test1", () => {
-  expect(func({ model: "Toyota", year: 2014 })).toEqual({
-    somethingGoesHere: 11614,
+// "Func" should be your function name.. Alternatively you can put your function in a seperate file,
+// change the name of the function and import it into your test.js file so your test will run properly.
+
+describe("Api1 test functions", () => {
+  test("test1", () => {
+    expect(func({ variable: "data", year: 2014 })).toEqual({
+      outcomeTitle: "expected result",
+    });
+  });
+
+  it("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).toBe(3);
   });
 });
+
+//! Jest test commands and functions
+
+// test() or it() defines a single unit test
+// expect() - Used to create an asserton so you can compare an actual value to the expected value
+
+// Matchers (Used with expect())
+// .toBe() - Strict equality (like ===) - expect(2 + 2)toBe(4)
+// .toEqual() - Deep equality (for object/array) - expect({a:1}).toEqual({a:1})
+// .not.toBe() - Negation - expect(2+2).not.toBe(5)
+// .toBeNull() - Checks for null - expect(null).toBeNull()
+// .toBeUndefined() - Checks for undefined - expect(undefined).toBeUndefined()
+// .toBeTruthy() - Value evaluates to true - expect("Hello").toBeTruthy()
+// .toBeFalsy() - Value to evaluate to false - expect(0).toBeFalsy()
+// .toContain() - Checks for array or string inclusion - expect([1,2,3]).toContain(2)
+// .toHaveLength() - Checks for length of array or string - expect("abc").toHaveLength(3)
+// .toThrow() - Expects function to throw an error - expect(() => {throw me an error();}).toThrow()
